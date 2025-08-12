ab23cefca85aa324cb4aea3082cd9c278b9bceab
Maria Eduarda Oliveira Machado <omariaeduarda411@gmail.com>
2025-08-12 10:11:24 -0300

    Rotas de animal

--- src/routes/animalRoutes.ts
+++ src/routes/animalRoutes.ts
@@ -9,7 +9,9 @@ animalRouter.post('/animais', authMiddleware, animalController.create);
 animalRouter.get('/animais', authMiddleware, animalController.findAll);
 animalRouter.get('/animais/:id', authMiddleware, animalController.findById);
 animalRouter.put('/animais/:id', authMiddleware, animalController.update);
-animalRouter.delete('/animais/:id/deactivate', authMiddleware, animalController.deactivate);
+//animalRouter.delete('/animais/:id/deactivate', authMiddleware, animalController.deactivate);
+
+animalRouter.delete('/animais/:id', authMiddleware, animalController.deactivate);
 
 animalRouter.get('/animais/:id_animal/consultas', authMiddleware, consultaController.buscarConsultasDoAnimal);
 
