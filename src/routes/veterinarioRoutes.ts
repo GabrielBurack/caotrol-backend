7c412d8f90f1b50c636b8fb53e61fa70c54a4545
GabrielBurack <gabrielburackrosa08@gmail.com>
2025-09-15 22:03:58 -0300

    fix: corrige permissoes para vet routes

--- src/routes/veterinarioRoutes.ts
+++ src/routes/veterinarioRoutes.ts
@@ -6,8 +6,8 @@ import adminAuthMiddleware from '../middlewares/adminAuthMiddleware';
 const veterinarioRouter = Router();
 
 veterinarioRouter.post('/veterinarios', authMiddleware, adminAuthMiddleware, veterinarioController.create);
-veterinarioRouter.get('/veterinarios', authMiddleware, adminAuthMiddleware, veterinarioController.findAll);
-veterinarioRouter.get('/veterinarios/:id', authMiddleware, adminAuthMiddleware,veterinarioController.findById);
+veterinarioRouter.get('/veterinarios', authMiddleware, veterinarioController.findAll);
+veterinarioRouter.get('/veterinarios/:id', authMiddleware,veterinarioController.findById);
 veterinarioRouter.put('/veterinarios/:id', authMiddleware,adminAuthMiddleware, veterinarioController.update);
 veterinarioRouter.delete('/veterinarios/:id', authMiddleware, adminAuthMiddleware, veterinarioController.delete);
 
