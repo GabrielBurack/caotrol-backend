9f5603dba486ab814a50416378b3adb58574bc68
Gabriel Burack Rosa <gabrielburackrosa08@gmail.com>
2025-10-23 11:55:35 -0300

    tirando auth de estados para teste

--- src/routes/estadoRoutes.ts
+++ src/routes/estadoRoutes.ts
@@ -6,7 +6,7 @@ import adminAuthMiddleware from '../middlewares/adminAuthMiddleware';
 const estadoRouter = Router();
 
 // A listagem de estados é uma rota que o frontend usará em formulários
-estadoRouter.get('/estados', authMiddleware, estadoController.findAll);
+estadoRouter.get('/estados', estadoController.findAll);
 
 // Apenas administradores podem criar novos estados
 estadoRouter.post('/estados', authMiddleware, adminAuthMiddleware, estadoController.create);
