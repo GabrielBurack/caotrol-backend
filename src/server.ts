ad4a92c145286dcbb9691de29298b88d86e4e6eb
Gabriel Burack Rosa <gabrielburackrosa08@gmail.com>
2025-10-23 10:36:21 -0300

    alterações para deploy

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ import enderecoRouter from "./routes/enderecoRoutes";
 import documentoRouter from "./routes/documentoRoutes";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
