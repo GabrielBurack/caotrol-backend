83740685a8338deee9a6d2c3ae7e86c957b48774
GabrielBurack <gabrielburackrosa08@gmail.com>
2025-11-02 09:27:27 -0300

    correçoes

--- src/server.ts
+++ src/server.ts
@@ -69,7 +69,7 @@ app.use("/api", cidadeRouter);
 app.use("/api", estadoRouter);
 app.use("/api", enderecoRouter);
 app.use("/api", documentoRouter);
-app.use('/api', helpRouter);
+app.use("/api", helpRouter);
 
 tarefasAgendadasService.iniciar();
 app.use(errorMiddleware);
